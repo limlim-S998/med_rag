cf7814d70bccc81ffc3de846607ca26180a85206
limlim <gzwhbosons@gmail.com>
2026-09-25 13:03:56 +1000

    Revert "Test PR failure reporting with a temporary failing check"
    This reverts commit 36caeaa90e385ede5be521b8709cb5d4e531e427.

--- tests/test_ci_signal.py
+++
@@ -1,4 +0,0 @@
-"""Temporary commissioning probe; removed after GitHub reports the failure."""
-
-def test_pr_reports_a_failed_check():
-    assert False, "Intentional PR status commissioning check"
